3694aac8b82f6b7eb92936a6da7f9e27fecb8356
KaloqnTodorov19 <kaloiantodorov12@gmail.com>
2023-12-04 11:18:51 +0200

    New Files

---
+++ WebShopAppp/WebShopAppp.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+using WebShopAppp.Infrastructure.Data.Domain;
+
+namespace WebShopAppp.Infrastructure.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+            this.Database.EnsureCreated();
+        }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
